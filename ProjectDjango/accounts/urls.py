764bef7379dd1cd82130483290055453faa9ae55
dolekhanhdang <dolekhanhdang@gmail.com>
2022-04-24 00:28:07 +0700

    Video 9.5

--- ProjectDjango/accounts/urls.py
+++ ProjectDjango/accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 urlpatterns = [
-    path('', views.home),
-    path('products/',views.products),
-    path('customers/<str:pk_test>/',views.customers),
+    path('', views.home,name="home"),
+    path('products/',views.products,name="products"),
+    path('customers/<str:pk_test>/',views.customers,name='customer'),
 ]
