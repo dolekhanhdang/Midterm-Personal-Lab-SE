56277f61d22c35c20bed8398b82784bca635e644
dolekhanhdang <dolekhanhdang@gmail.com>
2022-04-23 22:59:59 +0700

    Video 6
    add relationships between classes

--- ProjectDjango/accounts/models.py
+++ ProjectDjango/accounts/models.py
@@ -12,24 +12,36 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATELOGY = (
-        ('Indoor','Indoor'),
-        ('Outdoor','Outdoor')
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
     )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, null=True,choices=CATELOGY)
-    description = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATELOGY)
+    description = models.CharField(max_length=200, null=True,blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     STATUS = (
-        ('Pending','Pending'),
-        ('Out for delivery','Out for delivery'),
-        ('Delivered','Delivered')
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
     )
-    #customer =
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=200, null=True,choices=STATUS)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
