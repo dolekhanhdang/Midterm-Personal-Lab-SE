e5ac80c3c2443daa68d94432d75a1bd524e04aab
dolekhanhdang <dolekhanhdang@gmail.com>
2022-04-24 17:54:22 +0700

    Video 20

--- ProjectDjango/ProjectDjango/settings.py
+++ ProjectDjango/ProjectDjango/settings.py
@@ -129,3 +129,9 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
--- ProjectDjango/accounts/urls.py
+++ ProjectDjango/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
@@ -13,4 +14,15 @@ urlpatterns = [
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
     path('user/', views.userPage, name="user-page"),
     path('account/', views.accountSettings, name="account"),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html")
+         ,name='reset_password'),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent"
+                                                                                       ".html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<>/',auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form"
+                                                                                        ".html"),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name="accounts"
+                                                                                               "/password_reset_done.html"),name="password_reset_complete"),
+
 ]
